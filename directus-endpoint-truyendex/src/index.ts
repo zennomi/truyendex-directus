de96757fdbc9a3f3601150befdb5f414b5dd4953
zennomi <zennomi.kun@gmail.com>
2025-02-18 10:44:40 +0700

    feat: test proxy endpoint

--- directus-endpoint-truyendex/src/index.ts
+++ directus-endpoint-truyendex/src/index.ts
@@ -1,4 +1,6 @@
 import { defineEndpoint } from "@directus/extensions-sdk";
+import axios from "axios";
+import { HttpsProxyAgent } from "https-proxy-agent";
 
 export default defineEndpoint((router, context) => {
   router.get("/", (_req, res) => {
@@ -34,6 +36,44 @@ export default defineEndpoint((router, context) => {
         }
       });
       return res.json({ result });
+    } catch (error: any) {
+      return res
+        .status(500)
+        .json({ error: error.message || "Internal server error" });
+    }
+  });
+  router.get("/test-proxy", async (_req, res) => {
+    try {
+      if (!_req.query.proxyId) {
+        return res.status(400).json({ error: "Missing required parameters" });
+      }
+      const proxyId = _req.query.proxyId as string;
+
+      const proxy = await context
+        .database("proxy")
+        .where("id", proxyId)
+        .first();
+
+      if (!proxy) {
+        return res.status(404).json({ error: "Proxy not found" });
+      }
+
+      const url =
+        (_req.query.url as string) || "https://api.mangadex.org/manga";
+
+      const start = Date.now();
+      const { data } = await axios({
+        url,
+        httpsAgent: new HttpsProxyAgent(
+          proxy.username
+            ? `http://${proxy.username}:${proxy.password}@${proxy.ip}:${proxy.port}`
+            : `http://${proxy.ip}:${proxy.port}`
+        ),
+      });
+      const end = Date.now();
+      const responseTime = end - start;
+
+      return res.json({ responseTime });
     } catch (error: any) {
       return res
         .status(500)
